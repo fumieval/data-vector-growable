1f3398f466c373805fbc7fe7c9d30e6f2aa3e9a1
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-12-16 21:35:12 +0900

    polish the inteface

--- Data/Vector/Growable.hs
+++ Data/Vector/Growable.hs
@@ -1,12 +1,21 @@
 module Data.Vector.Growable
-  ( Growable(..)
-  , GVState(..)
+  ( Growable
+  -- * Type synonyms
+  , GrowableVector
+  , GrowableUnboxedVector
+  , GrowableStorableVector
+  , GrowableIOVector
+  , GrowableUnboxedIOVector
+  , GrowableStorableIOVector
+  -- * Operations
   , new
+  , withCapacity
   , replicate
   , replicateM
-  , pushBack
-  , popBack
+  , push
+  , pop
   , length
+  , null
   , read
   , write
   , modify
@@ -17,30 +26,51 @@ module Data.Vector.Growable
   , toGrowable
   ) where
 
-import Prelude hiding (read, length, replicate)
+import Prelude hiding (read, length, replicate, null)
+import Control.Exception
+import Control.Monad.Catch
 import Control.Monad.Primitive
 import Data.Bits
 import Data.Primitive.MutVar
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Generic.Mutable as MV
 import Data.Vector.Generic.Mutable (MVector)
+import qualified Data.Vector.Mutable as V
+import qualified Data.Vector.Unboxed.Mutable as U
+import qualified Data.Vector.Storable.Mutable as S
 
 data GVState v s a = GVState !Int !(v s a)
 
-newtype Growable v s a = Growable { unGrowable :: MutVar s (GVState v s a) }
+-- | 'Growable' is a dynamic vector based on mutable vector @v@.
+newtype Growable v s a = Growable (MutVar s (GVState v s a))
 
-new :: (PrimMonad m, MVector v a) => Int -> m (Growable v (PrimState m) a) 
-new len = do
-  vec <- MV.new len
-  Growable <$> newMutVar (GVState len vec)
+type GrowableVector = Growable V.MVector
+type GrowableUnboxedVector = Growable U.MVector
+type GrowableStorableVector = Growable S.MVector
+type GrowableIOVector = Growable V.MVector RealWorld
+type GrowableUnboxedIOVector = Growable U.MVector RealWorld
+type GrowableStorableIOVector = Growable S.MVector RealWorld
+
+-- | Create an empty vector with the given number of pre-allocated elements.
+withCapacity :: (PrimMonad m, MVector v a) => Int -> m (Growable v (PrimState m) a) 
+withCapacity cap = do
+  vec <- MV.new cap
+  Growable <$> newMutVar (GVState 0 vec)
+{-# INlINE withCapacity #-}
+
+-- | Create an empty vector
+new :: (PrimMonad m, MVector v a) => m (Growable v (PrimState m) a) 
+new = withCapacity 0
 {-# INLINE new #-}
 
+-- | Create a vector and fill with the initial value.
 replicate :: (PrimMonad m, MVector v a) => Int -> a -> m (Growable v (PrimState m) a)
 replicate len a = do
   vec <- MV.replicate len a
   Growable <$> newMutVar (GVState len vec)
 {-# INLINE replicate #-}
 
+-- | Like 'replicate', but initialises the elements by running the action repeatedly
 replicateM :: (PrimMonad m, MVector v a) => Int -> m a -> m (Growable v (PrimState m) a)
 replicateM len a = do
   vec <- MV.replicateM len a
@@ -48,64 +78,80 @@ replicateM len a = do
 {-# INLINE replicateM #-}
 
 -- | Append an element to the vector (not atomic).
-pushBack :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> a -> m ()
-pushBack (Growable ref) val = do
+push :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> a -> m ()
+push (Growable ref) val = do
   GVState len vec <- readMutVar ref
   vec' <- if MV.length vec == len
     then MV.unsafeGrow vec (max 1 len)
     else pure vec
   writeMutVar ref $ GVState (len + 1) vec'
   MV.write vec' len val
-{-# INLINE pushBack #-}
+{-# INLINE push #-}
 
-popBack :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> m (Maybe a)
-popBack (Growable ref) = do
+-- | Pop the last element. Returns 'Nothing' if the vector is empty.
+pop :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> m (Maybe a)
+pop (Growable ref) = do
   GVState len vec <- readMutVar ref
   if len == 0
     then pure Nothing
     else do
       writeMutVar ref $ GVState (len - 1) vec
       Just <$> MV.unsafeRead vec (len - 1)
-{-# INLINE popBack #-}
+{-# INLINE pop #-}
 
+-- | Get the length of the vector.
 length :: (PrimMonad m) => Growable v (PrimState m) a -> m Int
 length (Growable ref) = do
   GVState len _ <- readMutVar ref
   pure len
 {-# INLINE length #-}
 
-read :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> Int -> m a
+-- | Returns 'True' if the vector is empty
+null :: (PrimMonad m) => Growable v (PrimState m) a -> m Bool
+null (Growable ref) = do
+  GVState len _ <- readMutVar ref
+  pure $ len == 0
+{-# INLINE null #-}
+
+-- | May throw 'IndexOutOfBounds'
+read :: (PrimMonad m, MVector v a, MonadThrow m) => Growable v (PrimState m) a -> Int -> m a
 read (Growable ref) i = do
-  GVState _ vec <- readMutVar ref
-  MV.unsafeRead vec i
+  GVState len vec <- readMutVar ref
+  if i < len
+    then MV.unsafeRead vec i
+    else throwM $ IndexOutOfBounds $ show len <> " <= " <> show i
 {-# INLINE read #-}
 
-write :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> Int -> a -> m ()
+-- | Throws 'IndexOutOfBounds' if the index is larger than the size.
+write :: (PrimMonad m, MVector v a, MonadThrow m) => Growable v (PrimState m) a -> Int -> a -> m ()
 write (Growable ref) i val = do
-  GVState _len vec <- readMutVar ref
-  if MV.length vec <= i
-    then do
+  GVState len vec <- readMutVar ref
+  case len `compare` i of
+    LT -> throwM $ IndexOutOfBounds $ show len <> " < " <> show i
+    EQ -> do
       let amount = 1 `shiftL` (finiteBitSize i - countLeadingZeros i) - MV.length vec
       vec' <- MV.unsafeGrow vec amount
       MV.unsafeWrite vec' i val
       writeMutVar ref $ GVState (i + 1) vec'
-    else MV.unsafeWrite vec i val
+    GT -> MV.unsafeWrite vec i val
 {-# INLINE write #-}
 
-modify :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> (a -> a) -> Int -> m ()
+modify :: (PrimMonad m, MVector v a, MonadThrow m) => Growable v (PrimState m) a -> (a -> a) -> Int -> m ()
 modify (Growable ref) f i = do
   GVState len vec <- readMutVar ref
   if len <= i
-    then error $ "index out of bounds: " <> show len <> " <= " <> show i
+    then throwM $ IndexOutOfBounds $ show len <> " <= " <> show i
     else MV.unsafeModify vec f i
 {-# INLINE modify #-}
 
+-- | Thaw an immutable vector and create a 'Growable' one.
 thaw :: (G.Vector v a, PrimMonad m) => v a -> m (Growable (G.Mutable v) (PrimState m) a)
 thaw v = do
   vec <- G.thaw v
   Growable <$> newMutVar (GVState (G.length v) vec)
 {-# INLINE thaw #-}
 
+-- | Take a snapshot of a 'Growable' vector.
 freeze :: (G.Vector v a, PrimMonad m) => Growable (G.Mutable v) (PrimState m) a -> m (v a)
 freeze (Growable ref) = do
   GVState len vec <- readMutVar ref
@@ -113,6 +159,7 @@ freeze (Growable ref) = do
   pure $! G.unsafeTake len v
 {-# INLINE freeze #-}
 
+-- | Take a snapshot of a 'Growable' vector. The original vector may not be used.
 unsafeFreeze :: (G.Vector v a, PrimMonad m) => Growable (G.Mutable v) (PrimState m) a -> m (v a)
 unsafeFreeze (Growable ref) = do
   GVState len vec <- readMutVar ref
@@ -120,12 +167,14 @@ unsafeFreeze (Growable ref) = do
   pure $! G.unsafeTake len v
 {-# INLINE unsafeFreeze #-}
 
+-- | Turn 'Growable' vector into a regular mutable vector.
 fromGrowable :: (PrimMonad m, MVector v a) => Growable v (PrimState m) a -> m (v (PrimState m) a)
 fromGrowable (Growable ref) = do
   GVState len vec <- readMutVar ref
   pure $! MV.unsafeTake len vec
 {-# INLINE fromGrowable #-}
 
+-- | Create a 'Growable' vector from a mutable vector.
 toGrowable ::  (PrimMonad m, MVector v a) => v (PrimState m) a -> m (Growable v (PrimState m) a)
 toGrowable vec = Growable <$> newMutVar (GVState (MV.length vec) vec)
 {-# INLINE toGrowable #-}
---
+++ README.md
@@ -0,0 +1,9 @@
+data-vector-growable
+====
+
+![Haskell CI](https://github.com/fumieval/data-vector-growable/workflows/Haskell%20CI/badge.svg)
+[![Hackage](https://img.shields.io/hackage/v/data-vector-growable.svg)](https://hackage.haskell.org/package/data-vector-growable)
+[![Discord](https://img.shields.io/discord/664807830116892674?color=%237095ec&label=Discord&style=plastic)](https://discord.gg/DG93Tgs)
+
+This is a Haskell package that implements dynamic array (a.k.a. growable vector) based on any `vector` variants.
+See it on Hackage for more information.
