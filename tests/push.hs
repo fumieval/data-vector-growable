4f3315c8d7a131e071ba48851d9ab862007c3200
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-12-24 13:26:15 +0900

    make the test more legit

--- tests/push.hs
+++ tests/push.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE LambdaCase #-}
 import Data.Vector.Growable
-import qualified Data.Vector as BV
 import Control.Concurrent.Async (mapConcurrently_)
-import Control.Concurrent
+import Control.Concurrent (yield, threadDelay)
+import Control.Monad (unless)
 
 main :: IO ()
 main = do
@@ -10,7 +10,9 @@ main = do
   let step i = atomicPush vec i
   let popper count = atomicPop vec >>= \case
         Nothing -> yield >> popper count
-        Just (-1) -> print count
+        Just (-1) -> do
+            putStrLn $ "Count: " <> show count
+            unless (count == 5608) $ fail "this is not expected"
         Just _ -> popper $! count + 1
   mapConcurrently_ id
     $ popper (0 :: Int)
